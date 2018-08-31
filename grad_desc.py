fix normalization and exp column

grad_desc.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class modelo_linear:
 	# Initializer
-	def __init__(self,learning_rate=0.0003,train_iter=320000,mini_batch_len=100):
+	def __init__(self,learning_rate=0.001,train_iter=32000,mini_batch_len=100):
 		self.learning_rate = learning_rate
 		self.train_iter = train_iter
 		self.mini_batch_len = mini_batch_len
@@ -10,7 +10,7 @@ class modelo_linear:
 	# Fit parameters theta by mini-batch gradient descent
 	def fit(self,X_train,Y_train):
 		# Create column with 1
-		X_train = np.insert(X_train,0,1,1)
+		#X_train = np.insert(X_train,0,1,1)
 		# Initialize parameters with small random values
 		self.theta = np.random.normal(0.0,1.0,size=X_train[0].shape)
 		mean_losses = np.zeros(self.train_iter)
@@ -32,7 +32,7 @@ class modelo_linear:
 
 	# Estimate Y from X
 	def predict(self,X_test):
-		X_test = np.insert(X_test,0,1,1)
+		#X_test = np.insert(X_test,0,1,1)
 		return np.dot(X_test,self.theta)
 
 	# Compute mean squared loss

preprocess.py
@@ -42,6 +42,7 @@ def getXY(fl_path='diamonds-dataset/diamonds-train.csv'):
 											 "clarity0","clarity1","clarity2","clarity3","clarity4","clarity5","clarity6","clarity7",\
 											 "x","y","z","depth","table","price"] # One-hot mode
 	diamonds_df = pd.DataFrame(diamonds_table,columns=new_columns)
+	#diamonds_df["bias"] = 1.0
 	#diamonds_df["volume"] = diamonds_df["x"]*diamonds_df["y"]*diamonds_df["z"]
 	#diamonds_df["volume_2"] = diamonds_df["volume"]**2
 	#diamonds_df["volume_3"] = diamonds_df["volume"]**3
@@ -53,7 +54,8 @@ def getXY(fl_path='diamonds-dataset/diamonds-train.csv'):
 
 	# Normalize columns
 	diamonds_array = diamonds_df.values
-	diamonds_scaled = preprocessing.MinMaxScaler().fit_transform(diamonds_array)
+	diamonds_scaled = preprocessing.StandardScaler().fit_transform(diamonds_array)
+	#diamonds_scaled = preprocessing.MinMaxScaler().fit_transform(diamonds_array) # tirar min e max
 	diamonds_df = pd.DataFrame(diamonds_scaled,columns=list(diamonds_df))
 
 	# ["carat","cut","color","clarity","x","y","z","depth","table"]
@@ -63,8 +65,8 @@ def getXY(fl_path='diamonds-dataset/diamonds-train.csv'):
 	diamonds_Y = diamonds_df["price"].values
 
 	# Nova feature: e^(1.58*carat)
-	for x in diamonds_X:
-		x[0] = np.exp(1.58*x[0])
+	#for x in diamonds_X:
+	#	x[0] = np.exp(1.58*x[0])
 
 	return diamonds_X, diamonds_Y
 

test_prep.py
@@ -1,5 +1,6 @@
 # Lembrar de botar a coluna com 1
 
+import numpy as np
 from preprocess import getXY
 from grad_desc import *
 from sklearn import linear_model
@@ -8,17 +9,42 @@ from matplotlib import pyplot as plt
 
 train_X,train_Y = getXY()
 test_X, test_Y = getXY('diamonds-dataset/diamonds-test.csv')
+
+#train_X = np.random.uniform(-1.0,1.0,size=(100,10))
+#train_X = np.insert(train_X,0,1,1)
+#theta = np.random.normal(1.0,5.0,size=train_X.shape[1])
+#train_Y = np.dot(train_X,theta) + np.random.normal(0.0,0.1,size=100)
+
+#plt.plot(train_X,train_Y,'ro')
+#plt.show()
+
+#test_X = np.random.uniform(-1.0,1.0,size=(100,10))
+#test_X = np.insert(test_X,0,1,1)
+#test_Y = np.dot(test_X,theta) + np.random.normal(0.0,0.1,size=100)
+
+## (1) SGD regressor (sklearn)
 #sk_regressor = linear_model.SGDRegressor()
 #sk_regressor.fit(train_X,train_Y)
 #print(sk_regressor.score(test_X,test_Y))
 
+## (2) Descida de gradiente
 mod_l = modelo_linear()
 mse = mod_l.fit(train_X,train_Y)
 
-print(mse)
+#print(mse)
 plt.plot(mse)
 plt.show()
 
 Y_ = mod_l.predict(test_X)
 print(r2_score(test_Y,Y_))
+
+## (3) Equacao normal
+#X_T = np.transpose(train_X)
+#X_T_X = np.dot(X_T,train_X)
+#X_T_X_inv = np.linalg.inv(X_T_X)
+#X_T_X_inv_X_T = np.dot(X_T_X_inv,X_T)
+#theta = np.dot(X_T_X_inv_X_T,train_Y)
+
+#Y_ = np.dot(test_X,theta)
+#print(r2_score(test_Y,Y_))
 